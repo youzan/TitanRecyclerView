b93af49ec36b3691cc6347073f15c55130ef27af
liangfei <njliangfei@gmail.com>
2016-04-14 13:07:15 +0800

    create project

---
+++ sample/src/main/java/com/youzan/titan/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.youzan.titan.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
